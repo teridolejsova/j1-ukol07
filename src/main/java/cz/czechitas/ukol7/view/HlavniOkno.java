06db829059f72442b4492f9bfc67ee06674f8bb1
Tereza Dolejšová <terdoe>
2022-06-16 12:33:14 +0200

    2 problémy

--- src/main/java/cz/czechitas/ukol7/view/HlavniOkno.java
+++ src/main/java/cz/czechitas/ukol7/view/HlavniOkno.java
@@ -1,11 +1,16 @@
 package cz.czechitas.ukol7.view;
 
 import cz.czechitas.ukol7.Aplikace;
+import cz.czechitas.ukol7.Barva;
 import cz.czechitas.ukol7.controller.PreferenceController;
+import cz.czechitas.ukol7.formbuilder.FormBuilder;
+import cz.czechitas.ukol7.formbuilder.FormBuilderWithContainer;
+import cz.czechitas.ukol7.model.PreferenceBean;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.EnumMap;
 
 public class HlavniOkno extends JFrame {
 
@@ -28,5 +33,15 @@ public class HlavniOkno extends JFrame {
         setIconImage(new ImageIcon(Aplikace.class.getResource("czechitas-icon.png")).getImage());
         setLayout(new MigLayout("wrap 4", "[right, 100]rel[50:75:250,grow,fill]unrel[right, 100]rel[50:75:250,grow,fill]"));
         setMinimumSize(new Dimension(400, 200));
+
+        FormBuilderWithContainer<PreferenceBean> formbuilder = FormBuilder.create(controller.getModel())
+                .container(this);
+        formbuilder
+                .label("Přezdívka")
+                .textField("prezdivka")
+                .add();
+        formbuilder
+                .radioButton("Barva", "Barva",Barva)
+                .add();
     }
 }
